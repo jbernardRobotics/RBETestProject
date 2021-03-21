56b110374cbdd6a4999bafc236a295d37edd3ec4
jacob <jbernard@wpi.edu>
2021-03-21 17:29:05 -0400

    This is the first commit to a test project

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  digitalWrite(2, HIGH);
+  delay(1000);
+  digitalWrite(2, LOW);
+  delay(1000);
+}
