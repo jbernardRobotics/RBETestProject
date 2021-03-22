b19d2274d159a979201aaf1d073f447a1addadbf
jacob <jbernard@wpi.edu>
2021-03-21 20:35:46 -0400

    Added some more extensions. Added one foe better comments but it may conflict with another. I have to restart to see

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,12 @@ void loop()
   digitalWrite(2, HIGH);
   delay(1000);
   //Here is a test comment that will conflict
-  //Here is a different comment tha that shoult conflict
+  //Here is a different comment tha that should conflict
   //Blah Blah, Blah Main
   digitalWrite(2, LOW);
   delay(1000);
   Serial.println(String(helperTestFunction(2, 3)));
 
   //This stuff is an addition with no conflicts TODO: make sure to use these todo statements to keep track of stuff
-  //I'm going to make another branch and write over these comments
+  //I'm going to make another branch and write over these comments!
 }
